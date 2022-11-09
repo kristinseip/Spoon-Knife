53bdab6137aedb29a8f672b5ac8aab12d116f55e
KristinSeipStubbe <kristin.seip.stubbe@gmail.com>
2022-11-09 13:49:50 +0100

    updated with a class and code

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double[] intArr1 = new double[5];
+        intArr1[0] = 12.397;
+        intArr1[1] = 5346.3;
+        intArr1[2] = 90;
+        intArr1[3] = 6.58;
+        intArr1[4] = 368.11;
+
+    }
+}
